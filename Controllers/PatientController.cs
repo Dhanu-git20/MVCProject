d3ac489149799d21ed4de00d4bab2025a550cd19
Dhanu-git20 <dhanashrimajgaonkar@gmail.com>
2020-06-08 11:27:42 +0530

    My first Commit

---
+++ Controllers/PatientController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class PatientController : Controller
+    {
+        static List<PatientModel> patients = new List<PatientModel>();
+        public IActionResult Add()
+        {
+            return View("PatientAdd", patients);
+        }
+
+        public IActionResult Submit(PatientModel obj)
+        {
+            patients.Add(obj);
+            return View("PatientAdd", patients);
+           
+        }
+        
+    }
+}
